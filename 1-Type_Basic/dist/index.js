c77d96a6118438a1d6aad8af54fde89ba7ddcd94
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-01-19 23:35:05 +0700

    Function in TypeScript
    It's the same like in JavaScript, the main difference is that we must specify the data types of the paramater that it'll receive and the data type that the function will return.

--- 1-Type_Basic/dist/index.js
+++ 1-Type_Basic/dist/index.js
@@ -30,4 +30,34 @@ const f = fruits[3];
 let things = [1, true, "hello"];
 const t = things[0];
 // Objects
-let user;
+let user = {
+    firstName: "Syarifah Fadlun",
+    age: 21,
+    id: 69,
+};
+user.firstName = "Kujou Sara";
+user.id = 7;
+// Type inference with objects
+let person = {
+    name: "Mikasa",
+    score: 4,
+};
+/////////////////////////////////////[3 - Functions]
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumber = (a, b) => {
+    return a - b;
+};
+addTwoNumbers(3, 9);
+subtractTwoNumber(12, 3);
+function addAllNumbers(arrNum) {
+    const result = arrNum.reduce((curVal, accum) => curVal + accum, 0);
+    console.log(result);
+}
+addAllNumbers([1, 2, 3, 4, 5, 6, 7]);
+// return type inference
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+console.log(formatGreeting("Violet", "Hello"));
--- 1-Type_Basic/src/index.ts
+++ 1-Type_Basic/src/index.ts
@@ -38,4 +38,43 @@ let things = [1, true, "hello"];
 const t = things[0];
 
 // Objects
-let user: { firstName: string; age: number; id: number };
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "Syarifah Fadlun",
+  age: 21,
+  id: 69,
+};
+
+user.firstName = "Kujou Sara";
+user.id = 7;
+
+// Type inference with objects
+let person = {
+  name: "Mikasa",
+  score: 4,
+};
+
+/////////////////////////////////////[3 - Functions]
+function addTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const subtractTwoNumber = (a: number, b: number): number => {
+  return a - b;
+};
+
+addTwoNumbers(3, 9);
+subtractTwoNumber(12, 3);
+
+function addAllNumbers(arrNum: number[]): void {
+  const result = arrNum.reduce((curVal, accum) => curVal + accum, 0);
+  console.log(result);
+}
+
+addAllNumbers([1, 2, 3, 4, 5, 6, 7]);
+
+// return type inference
+function formatGreeting(name: string, greeting: string) {
+  return `${greeting}, ${name}`;
+}
+
+console.log(formatGreeting("Violet", "Hello"));
