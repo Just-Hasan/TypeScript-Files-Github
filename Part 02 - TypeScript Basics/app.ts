d486429c2a15034485a39fd67c8a9458a9155f12
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-05-02 23:01:32 +0700

    Update TypeScript Files Github

--- Part 02 - TypeScript Basics/app.ts
+++ Part 02 - TypeScript Basics/app.ts
@@ -1,9 +1,50 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// Object Types
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Hasan",
+//   age: 20,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "Author"],
+// };
+// person.role = [1, "Creator"]; -> work
+// person.role = [1, "Creator", 12]; -> won't work cuz role expect 2 element only
+// person.role[0] = "1" -> won't work cuz first index expect a number
+
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 7,
+  READ_ONLY,
+  AUTHOR,
 }
 
-const number1 = 5;
-const number2 = 2.8;
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: Role;
+} = {
+  name: "Hasan",
+  age: 20,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
 
-const result = add(number1, number2);
-console.log(result);
+let favouriteActivity: string[];
+favouriteActivity = ["Sports", "Coding"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("This person is an admin");
+} else {
+  console.log("This person is not an admin");
+}
