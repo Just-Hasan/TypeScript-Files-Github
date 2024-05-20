864ff2af7065fbddeccac82cc6354b0aded59a8a
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-05-20 16:06:48 +0700

    Update TypeScript Files Github

--- Part 06 - Advanced Types/dist/app.js
+++ Part 06 - Advanced Types/dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
+const errorBag = {
+    username: "Username is not valid",
+    email: "Email is not valid",
+};
 //# sourceMappingURL=app.js.map
---
+++ Part 06 - Advanced Types/src/app.ts
@@ -0,0 +1,12 @@
+////////[Index Properties]
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  username: "Username is not valid",
+  email: "Email is not valid",
+};
+
+////////[Function Overloads]
