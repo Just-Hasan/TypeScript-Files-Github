c7eee218731d966025cfae9d7889fca5b42b88a4
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-06-02 13:34:40 +0700

    Update TypeScript Files Github
    - Using Generics

--- Part 07 - Generics/dist/app.js
+++ Part 07 - Generics/dist/app.js
@@ -1,7 +1,66 @@
 "use strict";
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj1 = merge({ name: "Hasan" }, { age: 19 });
+console.log(mergedObj1);
 function sayHello(name, age) {
     return `I'm ${name} and i'm ${age}`;
 }
 const greet = sayHello("Hasan", 20);
 console.log(greet);
+function countAndDescribe(element) {
+    let description = `Got no value`;
+    if (element.length === 1) {
+        description = "Got one element";
+    }
+    else {
+        description = `Got ${element.length} elements`;
+    }
+    return [element, description];
+}
+console.log(countAndDescribe("Hello"));
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
+console.log(extractAndConvert({ name: "Hasan", address: ["Bjm", "Dasamaya"], age: 20 }, "name"));
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.splice(this.data.indexOf(item), 1)) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Hasan");
+textStorage.addItem("Kuro");
+textStorage.addItem("Pipiw");
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const names = ["Hasan", "Basri"];
+function testGeneric(a, b) {
+    return `Param 1 : ${a}\nParam 2 : ${b}`;
+}
+console.log(testGeneric("Hasan", 7));
+function testUnion(a) {
+    return `Param 1 ${a}`;
+}
+console.log(testUnion("Hasan"));
 //# sourceMappingURL=app.js.map
--- Part 07 - Generics/src/app.ts
+++ Part 07 - Generics/src/app.ts
@@ -8,7 +8,7 @@
 // promise.then((data) => data);
 
 // Generics in Function
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
@@ -26,3 +26,106 @@ function sayHello<T extends string, U extends number>(name: T, age: U) {
 //saat memanggil function, tentukan generic type yang akan dimasukan sebagai paramater
 const greet = sayHello("Hasan", 20);
 console.log(greet);
+
+interface Lengthy {
+  length: number;
+}
+// Kode dibawah berarti argument dari function harus memiliki property length
+// Karena countAndDescribe menggunakan interface Length sebagai type constraints
+// pada generic type nya
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = `Got no value`;
+  if (element.length === 1) {
+    description = "Got one element";
+  } else {
+    description = `Got ${element.length} elements`;
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe("Hello"));
+
+interface Person {
+  name: string;
+  age: number;
+  address: string[];
+}
+
+function extractAndConvert<T extends Person, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+console.log(
+  extractAndConvert(
+    { name: "Hasan", address: ["Bjm", "Dasamaya"], age: 20 },
+    "name"
+  )
+);
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  constructor() {}
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.splice(this.data.indexOf(item), 1)) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Hasan");
+textStorage.addItem("Kuro");
+textStorage.addItem("Pipiw");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objectStorage = new DataStorage<object>();
+// const hasanObj = { name: "Hasan" };
+// const alunObj = { name: "Alun" };
+// objectStorage.addItem({ name: "Hasan" });
+// objectStorage.addItem({ name: "Alun" });
+// objectStorage.removeItem(hasanObj);
+// console.log(objectStorage);
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date) {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Hasan", "Basri"];
+
+function testGeneric<T extends string, U extends number>(a: T, b: U) {
+  return `Param 1 : ${a}\nParam 2 : ${b}`;
+}
+
+console.log(testGeneric("Hasan", 7));
+
+function testUnion(a: string | number) {
+  return `Param 1 ${a}`;
+}
+
+console.log(testUnion("Hasan"));
