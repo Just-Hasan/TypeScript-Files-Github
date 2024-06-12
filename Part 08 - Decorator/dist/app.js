b627abaeab6ca9400c366ccd5df65c87a1e4eb2c
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-06-12 22:19:02 +0700

    update typescript files github

--- Part 08 - Decorator/dist/app.js
+++ Part 08 - Decorator/dist/app.js
@@ -1,2 +1,61 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function PropertyDecorator(target, propertyName) {
+    console.log(`Property ${propertyName}`);
+    console.log(target, propertyName);
+}
+function AccessorDecorator(target, accessorName, descriptor) {
+    console.log("Accessor Decorator");
+    console.log(target);
+    console.log(accessorName);
+    console.log(descriptor);
+}
+function MethodDecorator(target, targetName, descriptor) {
+    console.log("Method Decorator");
+    console.log(target);
+    console.log(targetName);
+    console.log(descriptor);
+}
+function ParamaterDecorator(target, name, position) {
+    console.log("Paramater Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    set changePriceTo(value) {
+        if (value > 0) {
+            this.price = value;
+        }
+        else {
+            throw new Error("You cannot have a negative number");
+        }
+    }
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    getPriceWithTax(tax) {
+        const priceTax = (this.price * tax) / 100;
+        return tax + priceTax;
+    }
+}
+__decorate([
+    PropertyDecorator
+], Product.prototype, "title", void 0);
+__decorate([
+    AccessorDecorator
+], Product.prototype, "changePriceTo", null);
+__decorate([
+    MethodDecorator,
+    __param(0, ParamaterDecorator)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- Part 08 - Decorator/src/app.ts
+++ Part 08 - Decorator/src/app.ts
@@ -1,3 +1,128 @@
-function add(a: number, b: number) {
-  return a + b;
+/*
+////////[Decorator]
+Decorator is a function that executes when the class
+is defined, decorator accepts one paramater, which is
+the constructor function of the class
+
+Decorator adalah function yang dieksekusi ketika suatu
+class di definisikan, decorator menerima satu paramater
+yaitu constructor function itu sendiri
+
+Perlu dimengerti bahwa decorator di eksekusi saat class
+di definisikan, bukan saat class di gunakan untuk membuat
+object
+*/
+
+/*
+////////[Decorator Factory]
+Decorator factory is basically a cool name for decorator
+that can accepts paramater. The closure concept is being
+applied here
+
+Decorator factory tuh cuman decorator yang bisa menerima
+paramater, konsep closure diterapkan disini
+*/
+
+// function Logger(constructor: Function) {
+//   console.log("Logging");
+//   console.log(constructor);
+// }
+
+// function Logger_DecoratorFactory(logString: string) {
+//   // konsep closure diterapkan disini
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// function WithTemplate(template: string, hookId: string) {
+//   return function (constructor: any) {
+//     console.log("rendering template");
+//     const hookEl = document.getElementById(hookId);
+//     const obj = new constructor();
+//     if (hookEl) {
+//       hookEl.innerHTML = template;
+//       hookEl.querySelector("h1")!.textContent = obj.name;
+//     }
+//   };
+// }
+
+// @Logger
+// @WithTemplate("<h2>Testing template</h2>", "app")
+// class Person {
+//   name = "Hasan";
+//   constructor() {
+//     // this.name = "Hasan";
+//     console.log("Creating person object");
+//   }
+// }
+
+// const person1 = new Person();
+
+// //
+
+function PropertyDecorator(target: any, propertyName: string | Symbol) {
+  console.log(`Property ${propertyName}`);
+  console.log(target, propertyName);
+}
+
+function AccessorDecorator(
+  target: any,
+  accessorName: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(accessorName);
+  console.log(descriptor);
+}
+
+function MethodDecorator(
+  target: any,
+  targetName: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(targetName);
+  console.log(descriptor);
+}
+
+function ParamaterDecorator(
+  target: any,
+  name: string | Symbol,
+  position: number
+) {
+  console.log("Paramater Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @PropertyDecorator
+  title: string;
+  // the log decorator will have the propertyName of title
+  private price: number;
+
+  @AccessorDecorator
+  set changePriceTo(value: number) {
+    if (value > 0) {
+      this.price = value;
+    } else {
+      throw new Error("You cannot have a negative number");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+
+  @MethodDecorator
+  getPriceWithTax(@ParamaterDecorator tax: number) {
+    const priceTax = (this.price * tax) / 100;
+    return tax + priceTax;
+  }
 }
